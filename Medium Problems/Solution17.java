9d8cd107e1f1828cdbb6364111f6e4dd649e73a2
Srideep Kundu <ksrideep007@gmail.com>
2025-07-09 21:53:33 +0530

    Day 9

---
+++ Medium Problems/Solution17.java
@@ -0,0 +1,13 @@
+//https://leetcode.com/problems/jump-game/description/
+
+class Solution17 {
+    public boolean canJump(int[] nums) {
+        int maxIndex = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(i > maxIndex) return false;
+            maxIndex = Math.max(maxIndex, i + nums[i]);
+        }
+        return true;
+    }
+}
